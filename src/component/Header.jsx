8693e521545d092e1578bd1615821752f18b2059
kavi nuansuta <kwinwlsutha@gmail.com>
2025-06-16 18:04:23 +0700

    create header component

---
+++ src/component/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function header() {
+  return (
+    <header>
+        <h1>Header component</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Nulla nostrum repellat possimus nobis hic neque eaque deserunt molestias excepturi autem debitis placeat exercitationem est architecto velit, 
+            iusto sed suscipit laborum?</p>
+    </header>
+  )
+}
+
+export default header
